bd041da66f9c0363bfb83e9ac7c09324a1ba52d0
Roberto <roblorus@icloud.com>
2022-12-14 23:59:53 +0100

    copy from development

--- src/api/server.py
+++ src/api/server.py
@@ -1,4 +1,4 @@
-"""This module implements the behaviour of the endpoints of the API"""
+'''This module implements the behaviour of the endpoints of the API'''
 import os
 import io
 import copy
@@ -10,7 +10,6 @@ from src.models.model import Model
 from src.api.services import do_predict, do_upload, evaluate_classification
 from fastapi.staticfiles import StaticFiles
 
-
 path_saved_model = os.path.join("models", "saved-model-optimal")
 
 ## remove the parameter cur_path if appears the error "No such file 'params.yaml'"
@@ -19,44 +18,37 @@ model = model.loadModel(path_saved_model)
 
 app = FastAPI()
 
-
 class ImageValidator(BaseModel):
-    """Pydantic validator for images"""
-
+    '''Pydantic validator for images'''
     # Same name as parameter of body request
     image: UploadFile
 
     @validator("image")
     def check_image(cls, image):
-        """Checks that the input file is actually an image"""
+        '''Checks that the input file is actually an image'''
         img = image.file.read()
         try:
             Image.open(io.BytesIO(img))
             image.file.close()
         except PIL.UnidentifiedImageError as exc:
-            raise ValueError(
-                "Image upload error, the file provided is not an image."
-            ) from exc
+            raise ValueError("Image upload error, the file provided is not an image.") from exc
         return img
 
-
 class LabelValidator(BaseModel):
-    """Pydantic validator for class labels"""
-
+    '''Pydantic validator for class labels'''
     val: int
 
     @validator("val")
     def check_val(cls, value):
-        """Checks that the provided label is between 0 and 9"""
+        '''Checks that the provided label is between 0 and 9'''
         if not 0 <= value < 10:
             raise ValueError(
                 "Id label error, the label must be a value between 0 and 9."
             )
 
-
 @app.post("/extend_dataset/")
 async def upload_file(imgfile: UploadFile, label: int):
-    """Upload an image in order to expand the dataset"""
+    '''Upload an image in order to expand the dataset'''
     try:
         LabelValidator(val=label)
         ImageValidator(image=copy.deepcopy(imgfile))
@@ -65,10 +57,9 @@ async def upload_file(imgfile: UploadFile, label: int):
     except ValidationError as exc:
         raise HTTPException(status_code=406, detail=str(exc.raw_errors[0].exc)) from exc
 
-
 @app.post("/classify_image/")
 async def predict(imgfile: UploadFile):
-    """Use the ml model in order to classify an image"""
+    '''Use the ml model in order to classify an image'''
     try:
         ImageValidator(image=copy.deepcopy(imgfile))
         res = await do_predict(imgfile, model)
@@ -76,10 +67,9 @@ async def predict(imgfile: UploadFile):
     except ValidationError as exc:
         raise HTTPException(status_code=406, detail=str(exc.raw_errors[0].exc)) from exc
 
-
 @app.put("/feedback_class/")
 async def eval_class(id_img: int, new_class: int):
-    """Allows experts to give the real label of an image already classified"""
+    '''Allows experts to give the real label of an image already classified'''
     try:
         LabelValidator(val=new_class)
         res = evaluate_classification(id_img, new_class)
